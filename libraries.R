a6d9ea224845093365d8398b15b5ed4f925b0342
Renad <renad.gharz@outlook.com>
2022-06-19 16:00:09 -0600

    checking model conditions

--- libraries.R
+++ libraries.R
@@ -1,5 +1,7 @@
 install.packages("ggplot2")
 install.packages("dplyr")
+install.packages("RColorBrewer")
 
 library(ggplot2)
 library(dplyr)
+library(RColorBrewer)
--- lm_conditions.R
+++ lm_conditions.R
@@ -1,1 +1,24 @@
-stores_scatter <- plot(stores)
+#Checking linearity
+plot(stores,
+     main="Scatterplot Matrix",
+     col="grey",
+     pch=4)
+cor(stores)
+
+#Checking equal variance
+raw_lm <- lm(data=stores,
+           formula=Store_Sales~Store_Area+Items_Available+Daily_Customer_Count)
+
+plot(
+  fitted(raw_lm), 
+  resid(raw_lm),
+  main = "Residuals vs Fitted Values",
+  xlab = "Fitted Values",
+  ylab = "Residuals")
+abline(0,0)
+
+#Checking normality
+qqnorm(raw_lm$residuals, 
+       datax = TRUE)
+qqline(raw_lm$residuals, 
+       datax = TRUE)
