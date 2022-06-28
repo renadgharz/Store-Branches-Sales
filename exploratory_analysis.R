476e729c8e6401cab24a0b9d621a9bb3e936bfa6
Renad <renad.gharz@outlook.com>
2022-06-28 12:24:09 -0600

    exploratory analysis

---
+++ exploratory_analysis.R
@@ -0,0 +1,5 @@
+stores_summary <- summary(stores)
+stores2_summary <- summary(stores2)
+
+boxplot(stores)
+boxplot(stores2)
--- import_validation.R
+++ import_validation.R
@@ -1,7 +1,5 @@
 stores <- read.csv("Stores.csv")
 stores <- stores %>% select(-ï..Store.ID)
 
-str(stores)
-summary(stores)
 
 stores2 <- log10(stores)
