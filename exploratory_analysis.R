bbccbdfe63fdaf4d9ffa528d90b8313cffd59797
Renad <renad.gharz@outlook.com>
2022-06-29 09:45:42 -0600

    data overview section

--- exploratory_analysis.R
+++ exploratory_analysis.R
@@ -1,7 +1,16 @@
+
 stores_summary <- summary(stores)
 boxplot(stores)
-hist(stores)
+
+boxplot(stores$Store_Area)
+boxplot(stores$Items_Available)
+boxplot(stores$Daily_Customer_Count)
+boxplot(stores$Store_Sales)
+
+hist(stores$Store_Area, breaks = 25)
+hist(stores$Items_Available, breaks = 25)
+hist(stores$Daily_Customer_Count, breaks = 25)
+hist(stores$Store_Sales, breaks = 25)
+
 
 stores2_summary <- summary(stores2)
-boxplot(stores2)
-hist(stores2)
